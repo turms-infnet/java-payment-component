0f8c92a2e90aeaec26b4b165f519663a7f55b5d5
Tiago Silva <tiagoluizribeirodasilva@gmail.com>
2025-05-13 21:55:04 -0300

    Update in payment method

--- src/main/java/dev/tiagosilva/service/PaymentService.java
+++ src/main/java/dev/tiagosilva/service/PaymentService.java
@@ -1,9 +1,9 @@
 package dev.tiagosilva.service;
 
 public class PaymentService {
-    public void processPayment(String paymentMethod) {
+    public void processPayment(String paymentMethod, Long value) {
         // Process the payment using the specified payment method
-        System.out.println("Processing payment using " + paymentMethod);
+        System.out.println("Processing payment using " + paymentMethod + " for amount: " + value);
     }
 
     public void refundPayment(String paymentMethod) {
